7e313342f3d9791dd726661770bdc080b28b7a4b
Mariola Pasko <paskomariola@gmail.com>
2019-10-02 10:42:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,37 +7,30 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  ["Moki", "Gabi", "Si", "Ilona"] 
- array.push ("Asia")
+ array.push 
 end
 
 def add_element_to_start_of_array(array, element)
- ["Moki", "Gabi", "Si", "Ilona"] 
-  array.unshift("Maria") 
+  array.unshift
 end
 
 def remove_element_from_end_of_array(array)
-  ["Moki", "Gabi", "Si", "Ilona"] 
   last_item = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  ["Moki", "Gabi", "Si", "Ilona"] 
   first_item = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  ["Moki", "Gabi", "Si", "Ilona"] 
   array[1]
 end
 
 def retrieve_first_element_from_array(array)
-  ["Moki", "Gabi", "Si", "Ilona"] 
   array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  ["Moki", "Gabi", "Si", "Ilona"] 
 x = array.count
   array[x-1] 
 end
