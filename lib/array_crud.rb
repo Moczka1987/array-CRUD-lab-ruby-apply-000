222c50d5ac31693db84b3076cfb5e8281a5e0451
Mariola Pasko <paskomariola@gmail.com>
2019-10-02 11:21:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,7 +34,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array = ["Moki", "am", "Si", "Asia", "arrays!"]
+  array = ["am", "`Gabi", "Si", "Asia", "arrays!"]
   array[0]
 end
 
