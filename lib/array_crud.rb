a32883639c6a56ce21ac06f39ff45923ae41be6f
Mariola Pasko <paskomariola@gmail.com>
2019-10-02 10:51:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,11 +15,11 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  last_item = array.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  first_item = array.shift
+   array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
@@ -31,5 +31,5 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-array.last 
+array[-1] 
 end
