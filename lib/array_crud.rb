1fa206ebd34b9b52a676b46938901467e2e61c5b
Mariola Pasko <paskomariola@gmail.com>
2019-10-02 10:30:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   ["Moki", "Gabi", "Si", "Ilona"] 
- create_an_array << "Asia"
+ add_element_to_end_of_array << "Asia"
 end
 
 def add_element_to_start_of_array(array, element)
