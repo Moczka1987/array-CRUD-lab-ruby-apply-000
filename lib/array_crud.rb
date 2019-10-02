9c41ab74da2312b7f77ca577244bee2ce59c82f4
Mariola Pasko <paskomariola@gmail.com>
2019-10-02 11:02:05 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
- ["Moki", "Gabi", "Si", "Ilona"] 
+ names ["Moki", "Gabi", "Si", "Asia"]
 end
 
 def add_element_to_end_of_array(array, element)
