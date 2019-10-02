0a35f72cde40ea98e3a55952ddb5a353b474ab5f
Mariola Pasko <paskomariola@gmail.com>
2019-10-02 11:09:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
- array ["Moki", "Gabi", "Si", "Asia"]
+ array = ["Moki", "Gabi", "Si", "Asia"]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -13,32 +13,32 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array ["Moki", "Gabi", "Si", "Asia"]
+  array = ["Moki", "Gabi", "Si", "Asia"]
   element = "Ilona"
   array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  array ["Moki", "Gabi", "Si", "Asia", "Ilona"]
+  array = ["Moki", "Gabi", "Si", "Asia", "Ilona"]
   array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  array ["Moki", "Gabi", "Si", "Asia", "Ilona"]
+  array = ["Moki", "Gabi", "Si", "Asia", "Ilona"]
    array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  array ["Moki", "Gabi", "Si", "Asia", "Ilona"]
+  array = ["Moki", "Gabi", "Si", "Asia", "Ilona"]
   index_number = array[1]
 end
 
 def retrieve_first_element_from_array(array)
-  array ["Moki", "Gabi", "Si", "Asia", "Ilona"]
+  array = ["Moki", "Gabi", "Si", "Asia", "Ilona"]
   array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  array ["Moki", "Gabi", "Si", "Asia", "Ilona"]
+  array = ["Moki", "Gabi", "Si", "Asia", "Ilona"]
 array[-1] 
 end
